3aa54cd504702db6961aca06f04cad8ddda7009f
NithishaSathishkumar <nithisha132004@gmail.com>
2025-01-24 20:09:42 -0800

    connected Camera access for the translation

--- SignTalkApp/src/app/(sections)/rtt/camera.tsx
+++ SignTalkApp/src/app/(sections)/rtt/camera.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useCameraPermission, useCameraDevice, Camera } from "react-native-vision-camera";
 
 const CameraScreen = () => {
+    const device = useCameraDevice('front');
+    const { hasPermission, requestPermission } = useCameraPermission();
+    
+    useEffect(() => {
+        if (!hasPermission) {
+            requestPermission();
+        }
+    }, [hasPermission]);
+
+    if (!hasPermission) {
+        return <ActivityIndicator />;
+    }
+    if (!device) {
+        return<Text>Camera Device not found</Text>
+    }
+
     return(
         <SafeAreaView>
             <Stack.Screen options={{ headerShown: false }}></Stack.Screen>
-            <Text>Camera Screen</Text>
+            <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
         </SafeAreaView> 
     
     );
