37ae9b010500a3d47e6e4e4f2a6dcd436ed04714
NithishaSathishkumar <nithisha132004@gmail.com>
2025-02-23 10:28:54 -0800

    change to server.py

--- SignTalkApp/server.py
+++ SignTalkApp/server.py
@@ -126,6 +126,6 @@ def predict():
     else: 
         return jsonify({'error': 'No hand landmarks detected'}) 
  
-    if __name__ == '__main__': 
-        app.run(host='0.0.0.0', port=5001) 
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5001) 
  
